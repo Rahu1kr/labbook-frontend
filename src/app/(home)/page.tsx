7b2b7b5ac63d155d729d6083893fe8700cc9dc98
Rahu1kr <88312053+Rahu1kr@users.noreply.github.com>
2024-08-14 19:05:19 +0530

    reorganize components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Banner from '@/app/(home)/components/Banner';
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to lbook.</h1>
+      <Banner />
     </>
   );
 }
